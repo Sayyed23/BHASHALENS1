101e0aacf7be1969d790819890f429eac4acdc7e
Ismail <ismailsayyed131@gmail.com>
2026-03-08 13:12:53 +0530

    feat: Add macOS plugin registrations and the package dependency lock file.

--- bhashalens_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ bhashalens_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -19,7 +19,6 @@ import flutter_secure_storage_macos
 import flutter_tts
 import google_sign_in_ios
 import package_info_plus
-import path_provider_foundation
 import record_macos
 import share_plus
 import shared_preferences_foundation
@@ -43,7 +42,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   RecordMacOsPlugin.register(with: registry.registrar(forPlugin: "RecordMacOsPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
